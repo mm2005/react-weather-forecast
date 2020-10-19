46e003725d56c9c831b155086d22dfe5d02d035d
Balint Molnar <balintkilencvenegy@gmail.com>
2020-10-19 18:42:55 +0200

    Rename ListSorter.js

--- src/components/favorites/FavoriteLocations.js
+++ src/components/favorites/FavoriteLocations.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import WeatherDetails from "../WeatherDetails";
 import axios from "axios";
 import FavoriteListContext from "../../context/FavoriteListContext";
-import Compare from "../../util/ListSorter";
+import Compare from "../../util/listSorter";
 
 const FavoriteLocations = () => {
   const apiKey = "3c850b0463346d2fffad82b66d5eb561";
